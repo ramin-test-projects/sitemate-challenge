71c9fcbedd07cbdf289031e791a4aef0aeb0428c
ramin <sir.raminyavari@gmail.com>
2024-06-06 19:48:14 +1000

    chore: Initialize the front-end project

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { IssuesPage } from "@/components/IssuesPage/IssuesPage";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <IssuesPage />
+    </div>
+  );
+}
---
+++ frontend/components/IssuesPage/IssuesPage.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const IssuesPage = () => {
+  return <div>Issues</div>;
+};
